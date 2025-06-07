f865105b5a077d16f5f66caec4c51e6b03e36a05
Priceton Braswell <pricetonbraswell@gmail.com>
2025-06-06 23:35:42 -0400

    removed build

--- client/build/_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/StencilMode.cpp.o.d
+++
@@ -1,6 +0,0 @@
-_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/StencilMode.cpp.o: \
- /home/pricetonb/code/sfml_udp_server_client/client/build/_deps/sfml-src/src/SFML/Graphics/StencilMode.cpp \
- /usr/include/stdc-predef.h \
- /home/pricetonb/code/sfml_udp_server_client/client/build/_deps/sfml-src/include/SFML/Graphics/StencilMode.hpp \
- /home/pricetonb/code/sfml_udp_server_client/client/build/_deps/sfml-src/include/SFML/Graphics/Export.hpp \
- /home/pricetonb/code/sfml_udp_server_client/client/build/_deps/sfml-src/include/SFML/Config.hpp
